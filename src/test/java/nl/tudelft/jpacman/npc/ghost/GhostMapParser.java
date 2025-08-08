dff63de53e5e2e69b5b866ac7feea429ad01ad34
Igor Chernenko <igorchernenko@uvic.ca>
2025-08-07 22:13:39 -0700

    Completed large tests for behavior, manual input modeling, and specification

--- src/test/java/nl/tudelft/jpacman/npc/ghost/GhostMapParser.java
+++ src/test/java/nl/tudelft/jpacman/npc/ghost/GhostMapParser.java
@@ -8,37 +8,30 @@ import nl.tudelft.jpacman.npc.Ghost;
 
 import java.util.List;
 
-/**
- * A test helper utility for writing the ghost unit tests.
- * A useful method for retrieving ghosts from your map would be:
- * findUnitInBoard in the Navigation class.
- */
+// this class helps put ghosts in maps for testing
 public final class GhostMapParser extends MapParser {
     private final GhostFactory ghostFactory;
 
-    /**
-     * Creates a new enhanced map parser.
-     * This map parser allows users to specify which ghost should be placed
-     * at an exact location.
-     *
-     * @param levelFactory The factory providing the NPC objects and the level.
-     * @param boardFactory The factory to create board elements.
-     * @param ghostFactory the factory to create the ghosts.
-     */
+    // makes a new map parser that can add ghosts
     public GhostMapParser(LevelFactory levelFactory, BoardFactory boardFactory,
                           GhostFactory ghostFactory) {
         super(levelFactory, boardFactory);
         this.ghostFactory = ghostFactory;
     }
 
-    //This method only supports clyde for now
-    //You should add extra cases for ghosts you need.
+    // puts stuff in the map depending on the letter
     @Override
     protected void addSquare(Square[][] grid, List<Ghost> ghosts,
                              List<Square> startPositions, int x, int y, char c) {
         switch (c) {
-            case 'C':
+            case 'B': // blinky
+                grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createBlinky());
+                break;
+            case 'C': // clyde
                 grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createClyde());
+                break;
+            case 'I': // inky
+                grid[x][y] = makeGhostSquare(ghosts, ghostFactory.createInky());
                 break;
             default:
                 super.addSquare(grid, ghosts, startPositions, x, y, c);
